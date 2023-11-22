a33a97c86fbb02cf347cf4a4b6b93306b0ab3919
Seongki Kim <moztiq@gmail.com>
2023-11-22 12:27:32 +0900

    setState

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class _AppState extends State<App> {
   int counter = 0;
 
   void onPressed() {
-    counter = counter + 1;
+    setState(() {
+      counter = counter + 1;
+    });
   }
 
   @override
